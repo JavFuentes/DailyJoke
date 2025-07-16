b33898f880f2265d9dedbe900c039eeba7a72f3e
Javfuentes <javier.antonio8128@gmail.com>
2025-07-16 02:11:08 -0400

    quité un par de importaciones innecesarias

--- app/src/main/java/dev/javfuentes/dailyjoke/viewmodel/JokeViewModel.kt
+++ app/src/main/java/dev/javfuentes/dailyjoke/viewmodel/JokeViewModel.kt
@@ -2,7 +2,6 @@ package dev.javfuentes.dailyjoke.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import dev.javfuentes.dailyjoke.data.JokeResponse
 import dev.javfuentes.dailyjoke.network.RetrofitInstance
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
